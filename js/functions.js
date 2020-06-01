883f26d03a7bb4e8e03fac76a015906ec967e8fa
Tomas <daugeltomas@gmail.com>
2020-06-01 13:48:29 +0300

    5# "Metų balansas"

--- js/functions.js
+++ js/functions.js
@@ -1,31 +1,44 @@
 function renderTables (data) {
     let HTML = '';
+    let totalIncome = 0;
+    let totalExpense = 0;
+    let totalBalance = 0;
 
     for (let i=0; i<data.length; i++) {
         let month = data[i];
         let balance = data[i].income - data[i].expense;
 
         if(data[i].income === undefined){
-            data[i].income = '-';
-            balance = 0 - data[i].expense + ' Eur';
+            data[i].income = '0';
+            balance = 0 - data[i].expense;
+            totalIncome += 0;
         } else {
-            data[i].income = data[i].income + ' Eur';
+            data[i].income = data[i].income;
+            totalIncome += data[i].income
         }
         if(data[i].expense === undefined){
-            data[i].expense = '-';
+            data[i].expense = '0';
             balance = data[i].income;
+            totalExpense += 0;
         } else {
-            data[i].expense = data[i].expense + ' Eur';
+            data[i].expense = data[i].expense;
+            totalExpense += data[i].expense;
         }
 
+
         HTML += `<div class="table-row">
-                    <div class="cell">${month.month}</div>
-                    <div class="cell">${months[month.month-1]}</div>
-                    <div class="cell">${month.income}</div>
-                    <div class="cell">${month.expense}</div>
-                    <div class="cell">${balance}</div>
+                    <div class="cell id">${month.month}</div>
+                    <div class="cell monthName">${months[month.month-1]}</div>
+                    <div class="cell income">${month.income} Eur</div>
+                    <div class="cell expense">${month.expense} Eur</div>
+                    <div class="cell balance">${balance} Eur</div>
                 </div>`;
+
+                totalBalance += balance;
     }
 
-    return document.querySelector('.table-content').innerHTML = HTML;
+    document.querySelector(".cell#totalIncome").innerHTML = totalIncome + ' Eur';
+    document.querySelector(".cell#totalExpenses").innerHTML = totalExpense + ' Eur';
+    document.querySelector(".cell#totalBalance").innerHTML = totalBalance + ' Eur';
+    document.querySelector('.table-content').innerHTML = HTML;
 } 
